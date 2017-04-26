502f8a009c21aa0cd1659cd905e61604b50ce42e
Schuyler Reinken <sreinken3@gatech.edu>
2017-04-26 14:03:49 -0400

    commdoop
    -

--- MainClass.java
+++ MainClass.java
@@ -2,6 +2,7 @@ class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello.");
         System.out.println("Marble.");
+        SecondClass thing = new SecondClass();
     }
     public static void helper() {
         
